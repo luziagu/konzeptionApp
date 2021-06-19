c781a27e4067122d3d0c146a2491ef6c44e3066e
luziagu <Luziebee@gmail.com>
2021-06-19 10:35:32 +0200

    test

--- prototyp/client.ts
+++ prototyp/client.ts
@@ -2,34 +2,34 @@ namespace highfive { // name space to isolate identifiers from other examples
 
     export interface ClientMessage {
         text: string;
-      }
+        }
 
-      window.addEventListener("load", createInput);
-      window.addEventListener("load", createInputPW);
+        window.addEventListener("load", createInput);
+        window.addEventListener("load", createInputPW);
    
-      function createInput():void {
+        function createInput():void {
         
-        let startBox: HTMLDivElement =<HTMLDivElement> document.getElementById("start-screen")
-        let NameInput: HTMLInputElement = document.createElement("input");
-                NameInput.type = "text";
-                NameInput.setAttribute("class","inputname");
-                NameInput.placeholder = "Type in your Name";
-                startBox.appendChild(NameInput) 
+          let startBox: HTMLDivElement =<HTMLDivElement> document.getElementById("start-screen")
+          let NameInput: HTMLInputElement = document.createElement("input");
+              NameInput.type = "text";
+              NameInput.setAttribute("class","inputname");
+              NameInput.placeholder = "Type in your Name";
+              startBox.appendChild(NameInput) 
 
                            
-      }
+        }
 
-      function createInputPW():void {
+        function createInputPW():void {
         
-        let startBox: HTMLDivElement =<HTMLDivElement> document.getElementById("start-screen")
-        let Password: HTMLInputElement = document.createElement("input");
+          let startBox: HTMLDivElement =<HTMLDivElement> document.getElementById("start-screen")
+          let Password: HTMLInputElement = document.createElement("input");
                 Password.type = "password";
                 Password.setAttribute("class","password");
                 Password.placeholder = "Type in your password";
                 startBox.appendChild(Password) 
 
                            
-      }
+        }
 
       
 
